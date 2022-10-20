e21f2ced89f8e27790aae24357fc2665a7c33d06
Noxiuam <96212795+Noxiuam@users.noreply.github.com>
2022-10-20 18:17:43 -0400

    decencies bithc

--- src/main/java/gq/noxiuam/bitches/Main.java
+++ src/main/java/gq/noxiuam/bitches/Main.java
@@ -81,6 +81,7 @@ public class Main {
         int amountOfBitches = this.allMyBitches.size();
 
         this.allMyBitches.add(new Gami(true));
+        this.allMyBitches.add(new Decencies(false));
 
         if (installedCheatBreakerSkid && amountOfBitches == 69) {
             this.allMyBitches.add(new Emily(true, true));
---
+++ src/main/java/gq/noxiuam/bitches/object/type/special/Decencies.java
@@ -0,0 +1,19 @@
+package gq.noxiuam.bitches.object.type.special;
+
+import gq.noxiuam.bitches.object.data.PCType;
+import gq.noxiuam.bitches.object.type.CyberBitch;
+
+/**
+ * @author Noxiuam
+ * https://noxiuam.gq
+ */
+public class Decencies extends CyberBitch {
+
+    private final boolean whore;
+
+    public Decencies(boolean whore) {
+        super(PCType.PREBUILT, true);
+        this.whore = whore;
+    }
+
+}
