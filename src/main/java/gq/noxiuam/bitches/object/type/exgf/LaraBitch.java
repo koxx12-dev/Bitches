36a95b68b119ddd28959977d4818edbb9b62e681
Maria <96212795+Noxiuam@users.noreply.github.com>
2022-12-05 15:50:42 -0500

    so glad she’s gone

--- src/main/java/gq/noxiuam/bitches/object/type/exgf/LaraBitch.java
+++ src/main/java/gq/noxiuam/bitches/object/type/exgf/LaraBitch.java
@@ -9,6 +9,7 @@ import gq.noxiuam.bitches.object.type.ExGirlfriendBitch;
 public class LaraBitch extends ExGirlfriendBitch {
 
     public LaraBitch() {
+        // in other words, never. these kinds of people never change.
         super(true, "changed their ways and stopped toying with people's emotions");
     }
 
